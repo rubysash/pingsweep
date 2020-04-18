bf0e2fa6ac3b79bf6d94d7d906c6139c9d686215
James Fraze <james@rubysash.com>
2020-04-18 11:08:42 -0500

    Added timer and fixed colorization

--- st.pingsweep.py
+++ st.pingsweep.py
@@ -1,6 +1,9 @@
 # going to hide the windows spawned
 import subprocess
 
+# let's time our script
+import time                 
+
 # parse out the CIDR to a list
 import ipaddress
 
@@ -10,6 +13,9 @@ init()
 
 # Prompt the user to input a network address
 net_addr = input("Enter Network (192.168.1.0/24): ")
+
+# actual code start time
+start = time.time()
 
 # Create the network
 ip_net = ipaddress.ip_network(net_addr)
@@ -26,11 +32,13 @@ info.wShowWindow = subprocess.SW_HIDE
 # run the ping command with subprocess.popen interface
 # though it's much faster to use a small number like 15ms
 # you'll miss hosts that take a long time on their response
-# so I've chosen 500ms response
+count = 0
 for i in range(len(all_hosts)):
-    output = subprocess.Popen(['ping', '-n', '1', '-w', '15', str(all_hosts[i])], stdout=subprocess.PIPE, startupinfo=info).communicate()[0]
+    output = subprocess.Popen(['ping', '-n', '1', '-w', '300', 
+        str(all_hosts[i])], stdout=subprocess.PIPE, 
+        startupinfo=info).communicate()[0]
     
-    # normalize colors to grey
+    # normalize colors
     print('\033[93m', end='')
 
     if "Destination host unreachable" in output.decode('utf-8'):
@@ -40,3 +48,12 @@ for i in range(len(all_hosts)):
     else:
         # print colors in green if online
         print(str(all_hosts[i]), '\033[32m'+"is Online")
+        count += 1
+
+
+# normalize colors
+print('\033[93m', end='')
+
+# ok, give us a final time report to 2 decimal places
+runtime = float("%0.2f" % (time.time() - start))
+print(count, "hosts responded to ping in", runtime, "seconds")
